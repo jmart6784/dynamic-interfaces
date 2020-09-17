e197853611d4eaee9c1749441aebf2108378fd99
Juan <jmartinez6784@yahoo.com>
2020-09-16 23:54:41 -0700

    Added image slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Menu
 let dropUp = document.getElementById("drop-up-hide");
 let mainHover = document.getElementById("hover-maintain");
 
@@ -7,4 +8,71 @@ mainHover.addEventListener("mouseenter", function () {
 
 mainHover.addEventListener("mouseleave", function () {
   dropUp.style.display = "none";
+});
+
+// Photo Slider
+let photoDiv = document.getElementById("photo-div");
+let photos = document.querySelectorAll(".slide");
+let prevBtn = document.getElementById("previous");
+let nextBtn = document.getElementById("next");
+
+// photos.forEach((photo) => {
+
+// });
+
+// Show first photo before set interval starts
+photos[0].style.display = "inline";
+
+let index = 0;
+
+let slideInt = setInterval(() => {
+  photos[index].style.display = "inline";
+
+  photos.forEach((pic) => {
+    if (pic === photos[index]) {
+      photos[index].style.display = "inline";
+    } else {
+      pic.style.display = "none";
+    }
+  });
+
+  if (index === photos.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+}, 5000);
+
+nextBtn.addEventListener("click", function () {
+  if (index === photos.length - 1) {
+    index = 0;
+    photos.forEach((pic) => {
+      pic.style.display = "none";
+    });
+    photos[index].style.display = "inline";
+  } else {
+    index++;
+
+    photos.forEach((pic) => {
+      pic.style.display = "none";
+    });
+    photos[index].style.display = "inline";
+  }
+});
+
+prevBtn.addEventListener("click", function () {
+  if (index === 0) {
+    index = photos.length - 1;
+    photos.forEach((pic) => {
+      pic.style.display = "none";
+    });
+    photos[index].style.display = "inline";
+  } else {
+    index--;
+
+    photos.forEach((pic) => {
+      pic.style.display = "none";
+    });
+    photos[index].style.display = "inline";
+  }
 });
